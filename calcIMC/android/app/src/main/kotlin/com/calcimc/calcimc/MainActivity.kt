785345e1c28103310e9ee818a2db374a08b136d5
Alberto Goncalves <jjunior666@gmail.com>
2023-04-26 21:27:05 -0300

    Subindo apps em desenvolvimento e estudo

---
+++ calcIMC/android/app/src/main/kotlin/com/calcimc/calcimc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.calcimc.calcimc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lojavirtual/android/app/src/main/java/br/com/agvendas/lojavirtual/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.agvendas.lojavirtual;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
